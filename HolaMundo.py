b442948d08f93432d1cf3b6ff923a348a444dc51
perspectivaerronea <91988911+perspectivaerronea@users.noreply.github.com>
2021-10-05 17:03:37 -0300

    Primer algo que hago en python

---
+++ HolaMundo.py
@@ -0,0 +1,21 @@
+import numpy as np
+
+a = 4
+b = 5
+c = a + b
+print(c)
+
+d = "Hola"
+e = "Mundo"
+f = d + " " + e
+print(f)
+
+g = [6,7,8,19,1,2,3,4,5]
+
+print(str(g[1]) + " <-- Esto demuestra que en Python los arrays comienzan en 0.")
+
+h = g
+
+h_list = np.array(h)
+
+print(h_list.array)
